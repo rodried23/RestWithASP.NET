c1218e6c4654985408c2b903ecdf4147c4cb32d3
douglas.eduardo <douglas.eduardo@tbfg.com.br>
2024-01-19 18:03:40 -0300

    Alteração no start e implementação de metodos

---
+++ 00_RestWithASPNET_ScaffoldViaVisualStudio/RestWithASPNET/Controllers/CalculatorController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstnumber}/{secondnumber}")]
+        public IActionResult Get(string firstnumber, string secondnumber)
+        {
+            if (IsNumeric(firstnumber) && IsNumeric(secondnumber))
+            {
+                var sum = ConvertToDecimal(firstnumber) + ConvertToDecimal(secondnumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid  Result");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+
+        }
+
+
+    }
+}
